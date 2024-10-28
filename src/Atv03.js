4287a646f84f43f53f2264d128cbba5068b73790
Leandro Lima <163360710+Mrlals@users.noreply.github.com>
2024-10-28 20:29:57 -0300

    Atividade Contato
    adicionado atividade 3 contador

---
+++ src/Atv03.js
@@ -0,0 +1,15 @@
+import Contador from "./Contador";
+import { Link } from "react-router-dom";
+
+function Atv03() {
+    return (
+      <>
+          <h2>Atividade 03</h2>
+          <Letreiro />
+
+          <Link to="/">Voltar</Link>
+      </>
+    );
+  }
+  
+  export default Atv03;
---
+++ src/Contador.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './Contador.css';
+
+function Contador() {
+  const [homens, setHomens] = useState(32);
+  const [mulheres, setMulheres] = useState(49);
+  const total = homens + mulheres;
+
+  const incrementarHomens = () => setHomens(homens + 1);
+  const decrementarHomens = () => setHomens(homens > 0 ? homens - 1 : 0);
+
+  const incrementarMulheres = () => setMulheres(mulheres + 1);
+  const decrementarMulheres = () => setMulheres(mulheres > 0 ? mulheres - 1 : 0);
+
+  const resetarContadores = () => {
+    setHomens(0);
+    setMulheres(0);
+  };
+
+  return (
+    <div className="contador-pessoas">
+      <h1>Total</h1>
+      <div className="total">
+        <input type="text" value={total} readOnly />
+        <button onClick={resetarContadores}>
+          <span role="img" aria-label="resetar">🔄</span>
+        </button>
+      </div>
+
+      <div className="contadores">
+        <div className="contador-homens">
+          <img src="/path-to-male-icon.png" alt="Homens" />
+          <div className="controls">
+            <button onClick={incrementarHomens} className="increment">
+              <span role="img" aria-label="incrementar">➕</span>
+            </button>
+            <button onClick={decrementarHomens} className="decrement">
+              <span role="img" aria-label="decrementar">➖</span>
+            </button>
+          </div>
+          <p>Homens</p>
+          <div className="count">{homens}</div>
+        </div>
+
+        <div className="contador-mulheres">
+          <img src="/path-to-female-icon.png" alt="Mulheres" />
+          <div className="controls">
+            <button onClick={incrementarMulheres} className="increment">
+              <span role="img" aria-label="incrementar">➕</span>
+            </button>
+            <button onClick={decrementarMulheres} className="decrement">
+              <span role="img" aria-label="decrementar">➖</span>
+            </button>
+          </div>
+          <p>Mulheres</p>
+          <div className="count">{mulheres}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contador;
